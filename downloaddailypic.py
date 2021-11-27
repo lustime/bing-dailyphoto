edc10e9bddd3b420d391904a8bf6cf4b8958e24c
lustime <2247868358@qq.com>
2021-11-27 13:29:56 +0800

    分支优化

--- downloaddailypic.py
+++ downloaddailypic.py
@@ -2,12 +2,20 @@ import requests
 import os
 from dailyphoto import get_today_image
 from dailyphoto import headers
+from dailyphoto import read_bing
 
 path = './'
-image = get_today_image()
-pic = requests.get(image.url, headers=headers)
-if not os.path.exists(path + 'pic'):
-    os.makedirs(path + 'pic')
-f = open(path + 'pic/' + '%s.%s' % (image.date, image.url[image.url.rindex('id=') + 3:]), 'wb')
-f.write(pic.content)
-f.close()
+# image = get_today_image()
+# pic = requests.get(image.url, headers=headers)
+# if not os.path.exists(path + 'pic'):
+#     os.makedirs(path + 'pic')
+# f = open(path + 'pic/' + '%s.%s' % (image.date, image.url[image.url.rindex('id=') + 3:]), 'wb')
+# f.write(pic.content)
+# f.close()
+for image in read_bing():
+    pic = requests.get(image.url, headers=headers)
+    if not os.path.exists(path + 'pic'):
+        os.makedirs(path + 'pic')
+    f = open(path + 'pic/' + '%s.%s' % (image.date, image.url[image.url.rindex('id=') + 3:]), 'wb')
+    f.write(pic.content)
+    f.close()
